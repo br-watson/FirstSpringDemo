2f655c7024fb413397848e56a5b43d934c2ef395
br-watson <bradleyrwatson@hotmail.com>
2023-09-18 11:59:28 +0100

    Added second hello

--- src/main/java/com/sky/dog/demo/rest/DogController.java
+++ src/main/java/com/sky/dog/demo/rest/DogController.java
@@ -1,6 +1,7 @@
 package com.sky.dog.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class DogController {
     @GetMapping("/hello") // 'maps' this method to a GET request at /hello
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/helloyou")
+    public String helloYou(@RequestParam(name = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
     }
 }

217812049c79f841b1df86cc64459ca12bea3e74
br-watson <bradleyrwatson@hotmail.com>
2023-09-18 10:55:42 +0100

    first commit

---
+++ src/test/java/com/sky/dog/demo/DogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.dog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

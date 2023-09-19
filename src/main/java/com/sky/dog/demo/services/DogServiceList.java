45085a8565a4fb757a0bb738525688aa6b400485
br-watson <bradleyrwatson@hotmail.com>
2023-09-19 10:21:55 +0100

    Added service annotation

--- src/main/java/com/sky/dog/demo/services/DogServiceList.java
+++ src/main/java/com/sky/dog/demo/services/DogServiceList.java
@@ -1,10 +1,12 @@
 package com.sky.dog.demo.services;
 
 import com.sky.dog.demo.domain.Dog;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class DogServiceList implements DogService {
 
     private final List<Dog> dogs = new ArrayList<>();
